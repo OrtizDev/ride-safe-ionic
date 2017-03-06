ff599604feb1a71f04e5cc055f7426c3f1b85338
startbluesoft <ziranda.chivas@gmail.com>
2017-03-06 12:48:18 -0600

    added, function to save SP2 values

--- js/services.js
+++ js/services.js
@@ -15,6 +15,33 @@ angular.module('app.services', [])
   };
 }])
 
+.factory('S2R', [ function (){
+    return {
+        setEstateS2R: function (IC, DR, AR, LT, MC) {
+            window.localStorage['S2R-IC'] = IC;
+            window.localStorage['S2R-DR'] = DR;
+            window.localStorage['S2R-AR'] = AR;
+            window.localStorage['S2R-LT'] = LT;
+            window.localStorage['S2R-MC'] = MC;
+        },
+        getEstateS2R_IC: function () {
+            return window.localStorage['S2R-IC'];
+        },
+        getEstateS2R_DR: function () {
+            return window.localStorage['S2R-DR'];
+        },
+        getEstateS2R_AR: function () {
+            return window.localStorage['S2R-AR'];
+        },
+        getEstateS2R_LT: function () {
+            return window.localStorage['S2R-LT'];
+        },
+        getEstateS2R_MC: function () {
+            return window.localStorage['S2R-MC'];
+        }
+    };
+}])
+
 .service('BlankService', [function(){
 
 }]);
