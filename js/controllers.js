5f21d4e2c80225103b73968fff66e4ebf0f571fb
Patsy <patsy@startbluesoft.com>
2017-03-09 13:38:18 -0600

    fixed, an extra comma in controllers file

--- js/controllers.js
+++ js/controllers.js
@@ -27,7 +27,7 @@ function ($scope, $stateParams) {
 .controller('loginCtrl', ['$scope', '$stateParams', '$state', 'UserSession',// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams, , $state, UserSession  ) {
+function ($scope, $stateParams, $state, UserSession  ) {
 
   $scope.formData = {};
   $scope.login = function () {
