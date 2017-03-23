2ef886de41f432e93628b782a086c1b6466a564b
Patsy <patsy@startbluesoft.com>
2017-03-22 18:41:44 -0600

    added, the view that will contain the navigation map in which now has the filter button, map and stop route button w/styles

--- js/controllers.js
+++ js/controllers.js
@@ -723,7 +723,7 @@ function ($scope, $stateParams) {
 
 }])
 
-.controller('crashesConfigCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('onRouteCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
--- js/routes.js
+++ js/routes.js
@@ -7,8 +7,8 @@ angular.module('app.routes', [])
   // Set up the various states which the app can be in.
   // Each state's controller can be found in controllers.js
   $stateProvider
-    
-  
+
+
 
       .state('menu.home', {
     url: '/home',
@@ -132,8 +132,13 @@ angular.module('app.routes', [])
     controller: 'closedAlertCtrl'
   })
 
+  .state('onRoute', {
+    url: '/on-route',
+    templateUrl: 'templates/onRoute.html',
+    controller: 'onRouteCtrl'
+  })
+
 $urlRouterProvider.otherwise('/left-menu/my-routes')
 
-  
 
 });
