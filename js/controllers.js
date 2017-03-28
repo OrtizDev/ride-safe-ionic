50ef75e01fe085018247f9da2388238f94a57bea
chivia17 <ziranda.chivas@gmail.com>
2017-03-28 13:11:06 -0600

    added, call the logout function

--- js/controllers.js
+++ js/controllers.js
@@ -242,11 +242,15 @@ function ($scope, $stateParams) {
 
 }])
 
-.controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('menuCtrl', ['$scope', '$stateParams', '$state', 'UserSession',// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $stateParams, $state, UserSession) {
 
+  $scope.logout = function() {
+    UserSession.clearUserData();
+    $state.go('login');
+  }
 
 }])
 
