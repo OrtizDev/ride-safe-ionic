c5bd2aed5bcec0042f368028f67e809e0c0a1598
Carlos Barranco <caba9313@gmail.com>
2017-04-10 00:54:25 -0500

    Maquetacion de descrubir rutas

--- platforms/android/assets/www/js/routes.js
+++ platforms/android/assets/www/js/routes.js
@@ -70,6 +70,51 @@ angular.module('app.routes', [])
         }
       })
 
+      // Caravana routes
+
+      .state('menu.caravana', {
+        url: '/caravana',
+        views: {
+          'side-menu21': {
+            templateUrl: 'templates/caravana/caravana.html',
+            controller: 'caravanaCtrl'
+          }
+        }
+      })
+
+      .state('menu.caravanaDestination', {
+        url: '/caravana/destination',
+        views: {
+          'side-menu21': {
+            templateUrl: 'templates/caravana/destination.html',
+            controller: 'destinationCaravanaCtrl'
+          }
+        }
+      })
+
+      .state('menu.caravanaCreateRoute', {
+        url: '/caravana/destination',
+        views: {
+          'side-menu21': {
+            templateUrl: 'templates/caravana/createRoute.html',
+            controller: 'createRouteCaravanaCtrl'
+          }
+        }
+      })
+
+      .state('menu.caravanaDiscoverRoute', {
+        url: '/caravana/discover',
+        views: {
+          'side-menu21': {
+            templateUrl: 'templates/caravana/discoverRoute.html',
+            controller: 'discoverRouteCaravanaCtrl'
+          }
+        }
+      })
+
+      // End of caravana's routes
+      // -----------------------------------------------------------
+
       .state('menu.sp2', {
         url: '/s2p',
         views: {
